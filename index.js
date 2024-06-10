eeee00fb8f3d3aca69061f479f57be6665879b8d
shRahat10 <shahriar.haque.1011@gmail.com>
2024-06-10 18:41:25 +0600

    deploy

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.use(cookieParser());
 app.use(cors({
   origin: [
     'http://localhost:5173',
+    'http://talent-syncro.web.app',
+    'http://talent-syncro.firebaseapp.com',
   ],
   credentials: true
 }));
